1fc577bf1ddf6a2adf29058b9347d709dcde4759
Kindinov Ivan <135726796+333B3@users.noreply.github.com>
2024-06-06 18:57:58 +0000

    end

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.example.fxdemo2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires MyBank;
+
+    opens org.example.fxdemo2 to javafx.fxml;
+    exports org.example.fxdemo2;
+}
